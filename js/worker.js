1b28032b97bbab11fdd175a30b147e81d671385d
Your Name <you@example.com>
2019-12-14 10:30:42 +0700

    addv11

--- js/worker.js
+++ js/worker.js
@@ -1,41 +1,30 @@
-// var CACHE_NAME = 'my-site-cache-v1';
-// var urlsToCache = [
-//   '/',
-//   '/1.css',
-//   '/js/1.js'
-// ];
-
-// self.addEventListener('install', function(event) {
-//   // Perform install steps
-//   event.waitUntil(
-//     caches.open(CACHE_NAME)
-//       .then(function(cache) {
-//         console.log('Opened cache');
-//         return cache.addAll(urlsToCache);
-//       })
-//   );
-// });
-// self.addEventListener('fetch', function(event) {
-//   event.respondWith(
-//     caches.match(event.request)
-//       .then(function(response) {
-//         // Cache hit - return response
-//         if (response) {
-//           return response;
-//         }
-//         return fetch(event.request);
-//       }
-//     )
-//   );
-// });
-onmessage = function(e) {
-  console.log('Worker: Message received from main script');
-  let result = e.data[0] * e.data[1];
-  if (isNaN(result)) {
-    postMessage('Please write two numbers');
-  } else {
-    let workerResult = 'Result: ' + result;
-    console.log('Worker: Posting message back to main script');
-    postMessage(workerResult);
-  }
+importScripts("/pusher.worker.js");
+// onmessage = function(e) {
+//   console.log('Worker: Message received from main script');
+//   let result = e.data[0] * e.data[1];
+//   if (isNaN(result)) {
+//     postMessage('Please write two numbers');
+//   } else {
+//     let workerResult = 'Result: ' + result;
+//     console.log('Worker: Posting message back to main script');
+//     postMessage(workerResult);
+//   }
+// }
+function demo(){
+	var pusher = new Pusher('726f4534d962542caa20', {
+		cluster: 'ap1'
+	});
+	var channel = pusher.subscribe('my-channel');
+	channel.bind('my-event', function(data) {
+		Push.create(data.name,{
+			body: data.message,
+			icon: 'images/logo-small.jpg',
+			timeout: 10000,
+			onClick: function () {
+				window.focus();
+				this.close();
+			}
+		});
+	});
+	alert('aaa');
 }
