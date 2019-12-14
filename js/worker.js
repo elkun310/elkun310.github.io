dd109904799b539aa3b0fa2c228a002150c7a438
Your Name <you@example.com>
2019-12-14 12:34:10 +0700

    addv11

--- js/worker.js
+++ js/worker.js
@@ -13,7 +13,7 @@ var pusher = new Pusher('726f4534d962542caa20', {
 var channel = pusher.subscribe('my-channel');
 
 channel.bind('my-event', function(data) {
-  self.registration.Push.create(data.name,{
+  self.registration.create(data.name,{
 					body: data.message,
 					icon: 'images/logo-small.jpg',
 					timeout: 10000,
