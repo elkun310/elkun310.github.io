bc64876ae4536f694cc93e222b584516beb80041
Your Name <you@example.com>
2019-12-14 12:22:09 +0700

    addv11

--- js/worker.js
+++ js/worker.js
@@ -1,30 +1,24 @@
 importScripts("/pusher.worker.js");
-onmessage = function(e) {
-  console.log('Worker: Message received from main script');
-  let result = e.data[0] * e.data[1];
-  if (isNaN(result)) {
-    postMessage('Please write two numbers');
-  } else {
-    let workerResult = 'Result: ' + result;
-    console.log('Worker: Posting message back to main script');
-    postMessage(workerResult);
-  }
-  function demo(){
-			// var pusher = new Pusher('726f4534d962542caa20', {
-			// 	cluster: 'ap1'
-			// });
-			// var channel = pusher.subscribe('my-channel');
-			// channel.bind('my-event', function(data) {
-			// 	Push.create(data.name,{
-			// 		body: data.message,
-			// 		icon: 'images/logo-small.jpg',
-			// 		timeout: 10000,
-			// 		onClick: function () {
-			// 			window.focus();
-			// 			this.close();
-			// 		}
-			// 	});
-			// });
-			 alert('aaa');
-		}
-}
+
+Pusher.setLogger(function(log){
+  console.log(log)
+});
+
+var pusher = new Pusher('726f4534d962542caa20', {
+  encrypted: true,
+  disableStats: true
+});
+
+var channel = pusher.subscribe('my-channel');
+
+channel.bind('my-event', function(data) {
+  self.registration.Push.create(data.name,{
+					body: data.message,
+					icon: 'images/logo-small.jpg',
+					timeout: 10000,
+					onClick: function () {
+						window.focus();
+						this.close();
+					}
+				});
+});
