8c47bd681454eabc178c16512548dc978595c85b
Your Name <you@example.com>
2019-12-14 10:33:57 +0700

    addv11

--- js/worker.js
+++ js/worker.js
@@ -10,21 +10,20 @@ importScripts("/pusher.worker.js");
 //     postMessage(workerResult);
 //   }
 // }
-function demo(){
-	var pusher = new Pusher('726f4534d962542caa20', {
-		cluster: 'ap1'
+var pusher = new Pusher('726f4534d962542caa20', {
+	cluster: 'ap1'
+});
+var channel = pusher.subscribe('my-channel');
+channel.bind('my-event', function(data) {
+	Push.create(data.name,{
+		body: data.message,
+		icon: 'images/logo-small.jpg',
+		timeout: 10000,
+		onClick: function () {
+			window.focus();
+			this.close();
+		}
 	});
-	var channel = pusher.subscribe('my-channel');
-	channel.bind('my-event', function(data) {
-		Push.create(data.name,{
-			body: data.message,
-			icon: 'images/logo-small.jpg',
-			timeout: 10000,
-			onClick: function () {
-				window.focus();
-				this.close();
-			}
-		});
-	});
-	alert('aaa');
-}
+});
+alert('aaa');
+
