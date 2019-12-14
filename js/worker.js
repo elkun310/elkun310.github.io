75a41772b02d5978bb489d890265e15f95554f69
Your Name <you@example.com>
2019-12-14 12:37:23 +0700

    addv11

--- js/worker.js
+++ js/worker.js
@@ -12,14 +12,14 @@ var pusher = new Pusher('726f4534d962542caa20', {
 
 var channel = pusher.subscribe('my-channel');
 
-channel.bind('my-event', function(data) {
-  self.registration.create(data.name,{
-					body: data.message,
-					icon: 'images/logo-small.jpg',
-					timeout: 10000,
-					onClick: function () {
-						window.focus();
-						this.close();
-					}
-				});
-});
+// channel.bind('my-event', function(data) {
+//   self.registration.Push.create(data.name,{
+// 					body: data.message,
+// 					icon: 'images/logo-small.jpg',
+// 					timeout: 10000,
+// 					onClick: function () {
+// 						window.focus();
+// 						this.close();
+// 					}
+// 				});
+// });
