90b7d166cc1e5ca691865e3e19a8a93cb03858dd
Your Name <you@example.com>
2019-12-14 21:16:03 +0700

    addv14

--- js/worker.js
+++ js/worker.js
@@ -9,22 +9,22 @@ var pusher = new Pusher('726f4534d962542caa20', {
   encrypted: true,
   disableStats: true
 });
-
 var channel = pusher.subscribe('my-channel');
-channel.bind('my-event', function(data) {
-  self.registration.showNotification(data.title, {
-    icon: 'https://vinno.vn/sites/default/files/favi_vinno_1.png',
-    body: data.message
-  });
-});
+
 // channel.bind('my-event', function(data) {
-//   self.registration.Push.create(data.name,{
-// 					body: data.message,
-// 					icon: 'images/logo-small.jpg',
-// 					timeout: 10000,
-// 					onClick: function () {
-// 						window.focus();
-// 						this.close();
-// 					}
-// 				});
+//   self.registration.showNotification(data.title, {
+//     icon: 'https://vinno.vn/sites/default/files/favi_vinno_1.png',
+//     body: data.message
+//   });
 // });
+channel.bind('my-event', function(data) {
+  self.registration.Push.create(data.name,{
+					body: data.message,
+					icon: 'images/logo-small.jpg',
+					timeout: 10000,
+					onClick: function () {
+						window.focus();
+						this.close();
+					}
+				});
+});
