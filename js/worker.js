e00b7aa5ce2163c097f599ee3e05d73ad3f8735e
Your Name <you@example.com>
2019-12-14 11:45:34 +0700

    addv11

--- js/worker.js
+++ js/worker.js
@@ -8,4 +8,22 @@ onmessage = function(e) {
     console.log('Worker: Posting message back to main script');
     postMessage(workerResult);
   }
+  function demo(){
+			// var pusher = new Pusher('726f4534d962542caa20', {
+			// 	cluster: 'ap1'
+			// });
+			// var channel = pusher.subscribe('my-channel');
+			// channel.bind('my-event', function(data) {
+			// 	Push.create(data.name,{
+			// 		body: data.message,
+			// 		icon: 'images/logo-small.jpg',
+			// 		timeout: 10000,
+			// 		onClick: function () {
+			// 			window.focus();
+			// 			this.close();
+			// 		}
+			// 	});
+			// });
+			 alert('aaa');
+		}
 }
