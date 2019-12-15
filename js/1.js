191d39220cba7aeb0e2a7b052aeb6051241d8b71
Your Name <you@example.com>
2019-12-15 14:13:47 +0700

    hieu ung scroll

--- js/1.js
+++ js/1.js
@@ -1,4 +1,13 @@
 ﻿ $(function(){
- 
+ 	var vitriedu = $('.edu').offset().top;
+ 	console.log(vitriedu);
+ 	$('.xuong').click(function(){
+ 		$('body,html').animate({scrollTop:vitriedu},500);
+ 	})
+ 	$('.btn-kn').click(function(){
+
+ 		$('body,html').animate({scrollTop:vitriedu},500);
+ 		return false;
+ 	})
 })  
  
