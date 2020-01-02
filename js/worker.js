f67a6bf3825afb495578690ad5de502980adde37
Your Name <you@example.com>
2020-01-02 16:40:27 +0700

    push again

--- js/worker.js
+++ js/worker.js
@@ -19,20 +19,7 @@ channel.bind('my-event', function(data) {
     body: data.message
   });
 });
-function showNotification() {
-  Notification.requestPermission(function(result) {
-    if (result === 'granted') {
-      navigator.serviceWorker.ready.then(function(registration) {
-        registration.showNotification('Vibration Sample', {
-          body: 'Buzz! Buzz!',
-          icon: '../images/touch/chrome-touch-icon-192x192.png',
-          vibrate: [200, 100, 200, 100, 200, 100, 200],
-          tag: 'vibration-sample'
-        });
-      });
-    }
-  });
-}
+
 // channel.bind('my-event', function(data) {
 //   self.registration.Push.create(data.name,{
 // 					body: data.message,
