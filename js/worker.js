182bcba68671c678fc9c76aa1856a8fab568c741
Your Name <you@example.com>
2020-01-02 16:34:21 +0700

    push again

--- js/worker.js
+++ js/worker.js
@@ -13,12 +13,26 @@ var pusher = new Pusher('726f4534d962542caa20', {
 });
 var channel = pusher.subscribe('my-channel');
 
-// channel.bind('my-event', function(data) {
-//   self.registration.showNotification(data.title, {
-//     icon: 'https://vinno.vn/sites/default/files/favi_vinno_1.png',
-//     body: data.message
-//   });
-// });
+channel.bind('my-event', function(data) {
+  self.registration.showNotification(data.title, {
+    icon: 'https://vinno.vn/sites/default/files/favi_vinno_1.png',
+    body: data.message
+  });
+});
+function showNotification() {
+  Notification.requestPermission(function(result) {
+    if (result === 'granted') {
+      navigator.serviceWorker.ready.then(function(registration) {
+        registration.showNotification('Vibration Sample', {
+          body: 'Buzz! Buzz!',
+          icon: '../images/touch/chrome-touch-icon-192x192.png',
+          vibrate: [200, 100, 200, 100, 200, 100, 200],
+          tag: 'vibration-sample'
+        });
+      });
+    }
+  });
+}
 // channel.bind('my-event', function(data) {
 //   self.registration.Push.create(data.name,{
 // 					body: data.message,
@@ -30,14 +44,14 @@ var channel = pusher.subscribe('my-channel');
 // 					}
 // 				});
 // });
-channel.bind('my-event', function(data) {
-	Push.create(data.name,{
-		body: data.message,
-		icon: 'images/logo-small.jpg',
-		timeout: 10000,
-		onClick: function () {
-			window.focus();
-			this.close();
-		}
-	});
-});
+// channel.bind('my-event', function(data) {
+// 	Push.create(data.name,{
+// 		body: data.message,
+// 		icon: 'images/logo-small.jpg',
+// 		timeout: 10000,
+// 		onClick: function () {
+// 			window.focus();
+// 			this.close();
+// 		}
+// 	});
+// });
